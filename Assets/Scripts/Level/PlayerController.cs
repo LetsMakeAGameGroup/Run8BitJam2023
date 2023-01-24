53af29f2cba9ff7628eefe54c2877305e4cae078
Dannan Vazquez <dannanvazquez@gmail.com>
2023-01-24 01:45:45 -0800

    Basic player movement
    PlayerMovement script to constantly run to the right, while being able to jump and sprint faster.

---
+++ Assets/Scripts/Level/PlayerController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int batteries = 0;
+    private int currentTemp = 50;
+}
---
+++ Assets/Scripts/Level/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkingSpeed = 10;
+    [SerializeField] private float runningSpeed = 15;
+    [SerializeField] private float jumpForce = 20;
+
+    private bool isGrounded = false;
+
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float currentSpeed = isRunning ? runningSpeed : walkingSpeed;
+
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.Raycast(transform.position, -Vector2.up, 0.6f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
