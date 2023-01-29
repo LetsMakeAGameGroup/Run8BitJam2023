79b594f3d4c40a35510a4e774d105e3857179218
Dannan Vazquez <dannanvazquez@gmail.com>
2023-01-29 11:21:37 -0800

    Added collide with obstacle SFX
    Colliding with the obstacle now makes a sound.

--- Assets/Scripts/Gameplay/Obstacle.cs
+++ Assets/Scripts/Gameplay/Obstacle.cs
@@ -7,13 +7,34 @@ public class Obstacle : MonoBehaviour {
     [SerializeField] private float slowSpeedPerc;
     [SerializeField] private float slowTime;
 
+    [SerializeField] private GameObject model;
+
+    private bool isPickedUp = false;
+
     private void OnTriggerEnter2D(Collider2D collision) {
+        if (isPickedUp) return;
+
         PlayerController playerController = collision.GetComponent<PlayerController>();
         if (!playerController) return;
 
         playerController.SetOffFire();
 
         collision.GetComponent<PlayerMovement>().ReduceCurrentSpeedByTime(slowSpeedPerc, slowTime);
+        StartCoroutine(CollideObstacle());
+    }
+
+    IEnumerator CollideObstacle() {
+        isPickedUp = true;
+        model.SetActive(false);
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.volume = (PlayerPrefs.HasKey("FXVolume") ? PlayerPrefs.GetFloat("FXVolume") / 100f : 0.5f);
+        audio.Play();
+
+        while (audio.isPlaying) {
+            yield return null;
+        }
+
         Destroy(gameObject);
     }
 }
